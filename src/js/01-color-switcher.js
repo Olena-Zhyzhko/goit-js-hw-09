bc2853d8e36944d9b5311660a2807d44513f2bde
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-29 00:57:26 +0300

    01 color-switcher - Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId;
+btnStop.disabled = true;
+
+
+btnStart.addEventListener('click', onListenerBtnStart);
+
+const timer = {
+    firstColor() {
+        return startId = setTimeout(() => {
+            body.style.backgroundColor = getRandomHexColor();
+        }, 0);
+    },
+
+    nextColor() {
+        return timerId = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    }        
+}  
+
+function onListenerBtnStart(event) {
+    btnStop.addEventListener('click', onListenerBtnStop);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    timer.firstColor();
+    timer.nextColor();
+}
+
+function onListenerBtnStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
