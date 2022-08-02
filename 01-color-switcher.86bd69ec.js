fce416f50da8ca5399cce519760b395e19dc480f
Olena-Zhyzhko <97410016+Olena-Zhyzhko@users.noreply.github.com>
2022-08-02 21:36:44 +0000

    Deploying to gh-pages from @ Olena-Zhyzhko/goit-js-hw-09@5d4125f355d658267bf373dcfcf9b4d8646c3f3a 🚀

---
+++ 01-color-switcher.86bd69ec.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=document.querySelector("body");n.disabled=!0,e.addEventListener("click",(function(o){e.disabled=!0,n.disabled=!1,t=setInterval((function(){return d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.86bd69ec.js.map
